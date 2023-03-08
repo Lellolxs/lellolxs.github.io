3f3ee90c95fcc0ddde86750a5248e2e61bede442
Granilla Péter <nagyonlol69@gmail.com>
2023-03-08 21:34:50 +0100

    finished the individual component volume calculation code, wrote some design ideas into index.html, improved precision of all calculations

--- lacinak/main.js
+++ lacinak/main.js
@@ -2,12 +2,12 @@ var fieldContainer = $("#fieldContainer");
 var resultField = $("#eredmeny");
 
 const newField = `
-<div class="border border-secondary rounded-4 p-3 pb-2 mb-3 text-light input-bg">
+<div class="border border-secondary rounded-4 p-3 pb-2 mb-3 text-light input-bg" id="test-asd">
     <button class="btn btn-danger closebutton" onclick="delField(this)">X</button>
     <div class="wrap justify-content-between">
         <div class="inputfields">
             <div style="width: fit-content;" class="mx-auto">
-                <label for="width" class="form-label">Szélesség</label>
+                <label for="width" class="form-label">Hosszúság</label>
                 <div class="input-group">
                     <input type="number" id="width" oninput="calcIndividual(this)" class="form-control calc-input">
                     <div class="input-group-append">
@@ -18,7 +18,7 @@ const newField = `
         </div>
         <div class="inputfields">
             <div style="width: fit-content;" class="mx-auto">
-                <label for="height" class="form-label">Magasság</label>
+                <label for="height" class="form-label">Szélesség</label>
                 <div class="input-group">
                     <input type="number" id="height" oninput="calcIndividual(this)" class="form-control calc-input">
                     <div class="input-group-append">
@@ -55,12 +55,11 @@ const newField = `
     </div>
 </div>
 `
-
 function delField(a){
     $(a).parent().remove();
+    // calculate();
+    // recalcPrice();
 }
-
-
 function formatMoney(number, decPlaces, decSep, thouSep) {
     decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
     decSep = typeof decSep === "undefined" ? "." : decSep;
@@ -91,8 +90,8 @@ function calculate() {
         sum += width*height*depth*count;
     }
     resultField.empty();
-    sum = Math.round(sum*10000)/10000;
-    resultField.append(`<span title="${sum}">Össz térfogat: ${sum} m<sup>3</sup></span>`);
+    //sum = Math.round(sum*10000)/10000;
+    resultField.append(`<span title="${Math.round(sum*100000)/100000}">Össz térfogat: ${Math.round(sum*100000)/100000} m<sup>3</sup></span>`);
     if (isNaN(parseFloat(r_cost.value))) {
 
     } else {
@@ -100,30 +99,35 @@ function calculate() {
     }
 }
 
-function recalcPrice(){
-    if (isNaN(parseFloat(r_cost.value))) {
-        return;
-    }
-    cost.innerText = `Összeg: ${formatMoney(sum*parseFloat(r_cost.value))} Ft.`
-}
+// function recalcPrice(){
+//     if (isNaN(parseFloat(r_cost.value))) {
+//         return;
+//     }
+//     cost.innerText = `Összeg: ${formatMoney(sum*parseFloat(r_cost.value))} Ft.`
+// }
 
 function addField() {
     fieldContainer.append($.parseHTML(newField));
 }
 
 function calcIndividual(e){
-    let localroot = $(e).closest('.current-field');
-    let individual = localroot.children('.individual');
-    let width = parseFloat(localroot.children('#width').value)/1000;
-    let height = parseFloat(localroot.children('#height').value)/1000;
-    let depth = parseFloat(localroot.children('#depth').value)/1000;
-    let count = parseFloat(localroot.children('#count').value);
+    let root = $(e).closest('#test-asd')[0];
+
+    let individual = root.querySelector('.individual');
+    let width = parseFloat(root.querySelector('#width').value)/1000;
+    let height = parseFloat(root.querySelector('#height').value)/1000;
+    let depth = parseFloat(root.querySelector('#depth').value)/1000;
+    let count = parseFloat(root.querySelector('#count').value);
+
+    // let count = parseFloat($(e).closest('.currentField').children('input'));
+    //console.log($(e).closest('.currentField').html());
+    //console.log($(e).val());
+    //console.log(dx[0].querySelector("#width").value)
     let volume = width*depth*height;
 
-    console.log(localroot)
+    //console.log(localroot)
     // console.log(localroot.children('.individual'));
-    console.log(localroot.children('#count').html());
+    //console.log(localroot.children('#count').html());
 
-    individual.empty();
-    individual.append(`<h5>1 db: ${Math.round(volume*10000)/10000} m<sup>3</sup>\t${count} db: ${Math.round(volume*count*10000)/10000} m<sup>3</sup></h5>`);
+    individual.innerHTML = `<h5>1 db: ${Math.round(volume*1000000)/1000000} m<sup>3</sup>\t${count} db: ${Math.round(volume*count*1000000)/1000000} m<sup>3</sup></h5>`
 }
